45a22940152eb70700c4d2623193c22c65a7b6f3
moinSebi <s.vorbrugg@googlemail.com>
2022-07-13 13:19:16 +0200

    Replace 0 collumns with TAIR10

--- scripts/anna2bed.py
+++ scripts/anna2bed.py
@@ -9,7 +9,7 @@ import sys
 
 def read_annafile(filename):
     """
-
+    Read the anna file content
     :param filename: File name
     :return: data [line.split()]
     """
@@ -20,6 +20,20 @@ def read_annafile(filename):
                 ls = line.split()
                 data.append(ls)
     return data
+
+def replace0(data):
+    """
+    Replace 0 with TAIR10
+    :param data: anna file content
+    :return:
+    """
+    data2 =  []
+    for x in data:
+        if str(x[3]) == "0":
+            data2.append([x[0], x[1], x[2], "TAIR10"])
+        else:
+            data2.append([x[0], x[1], x[2], x[3]])
+    return data2
 
 def write_output(filename, data):
     """
@@ -39,5 +53,11 @@ def write_output(filename, data):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="anna result file", required=True)
+    parser.add_argument("-r", "--replace", help = "Replace the 0 with TAIR10", action = "store_true")
     parser.add_argument("-o", "--out", help="output file")
+
     args = parser.parse_args()
+    data = read_annafile(args.input)
+    if args.replace:
+        data = replace0(data)
+    write_output(args.out, data)

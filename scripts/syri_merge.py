1079a192f5dc6ad431c7ae41b29b7d1062dd6bd9
moinSebi <s.vorbrugg@googlemail.com>
2022-07-14 16:34:59 +0200

    New write function and multiple small bugfixes.

--- scripts/syri_merge.py
+++ scripts/syri_merge.py
@@ -8,7 +8,7 @@ Comment:
 For not reading the whole data frame (99% is SNPs)
 --> grep -v "SNP" file.csv > file_noSNP.csv
 """
-
+import sys
 
 import pandas as pd
 import argparse
@@ -16,7 +16,7 @@ import numpy as np
 from scipy.cluster.hierarchy import linkage, fcluster
 
 
-def filter_syri(df1):
+def filter_syri(df):
     """
     Read a merged syri data frame (without SNPs)
     Filter criteria:
@@ -26,8 +26,7 @@ def filter_syri(df1):
     :param df1: Data frame 1
     :return: Filtered data frame
     """
-    # Remove alignment
-    df = df1.loc[df1[10].apply(lambda x: x[-2:] != "AL")]
+    print("Reading SYRI", file = sys.stderr)
 
     # Change data type to int
     df[1] = df[1].astype(int)
@@ -38,23 +37,19 @@ def filter_syri(df1):
     df[14] = df[14].astype(str)
 
 
-    # Remove SNPs
-    df = df.loc[df[10] != "SNP"]
-    # Remove SYN
-    df = df.loc[df[10] != "SYN"]
-    df = df.rename(columns={15: "sample"})
-    df = df.rename(columns={14: "ref"})
+
+
+    # Rename the columns
+    df = df.rename(columns={15: "ref"})
+    df = df.rename(columns={14: "sample"})
     df = df.rename(columns={13: "len_sample"})
     df = df.rename(columns={12: "len_ref"})
-
-    # Sort by length
-    df = df.loc[(df["len_sample"] >= 50) | (df["len_ref"] >= 50)]
     return df
 
 
 def readList(listname):
     """
-
+    Reading a list of accession
     :param listname: name of the list to read
     :return: list data structure of the list
     """
@@ -89,7 +84,6 @@ def clusterthis(ypred, t1, t2, l1, l2, lenn):
         if i < len(l1):
             if y in k:
                 k[y].append((t1.iloc[i].name, l1[i]))
-                print(t1.iloc[i].index)
             else:
                 k[y] = [(t1.iloc[i].name, l1[i])]
         else:
@@ -100,20 +94,44 @@ def clusterthis(ypred, t1, t2, l1, l2, lenn):
                 k[y] = [(t2.iloc[i2].name, l2[i2])]
     return k
 
+
+def clusterthis2(ypred, t1, t2, l1, l2, lenn):
+    """
+    :param ypred: cluster output
+    :param t1: DataFrame1
+    :param t2: DataFrame2
+    :param l1: values1
+    :param l2: values2
+    :param lenn: len of l1
+    :return: dict(clusternumb -> (name, [start, stop]))
+    """
+    k = dict()
+    for i, y in enumerate(ypred):
+        if i < len(l1):
+            if y in k:
+                k[y].append(i)
+            else:
+                k[y] = [(t1.iloc[i].name, l1[i])]
+        else:
+            i2 = i - lenn
+            if y in k:
+                k[y].append(i)
+            else:
+                k[y] = [(t2.iloc[i2].name, l2[i2])]
+    return k
+
 def cluster(alldf, l):
     """
     :param alldf: pandas DataFrame
     :param l: list
     :return:
     """
+    print("Clustering", file = sys.stderr)
     rr = dict()
 
     for y in l:
-        print(y)
         t1 = alldf.loc[alldf["sample"] == y].loc[alldf["len_sample"] != 0]
         t2 = alldf.loc[alldf["ref"] == y].loc[alldf["len_ref"] != 0]
-        print(len(alldf.loc[alldf["ref"] == y]))
-        print(t2)
         rr[y] = dict()
         for x in ["Chr1", "Chr2", "Chr3", "Chr4", "Chr5"]:
             dft1 = t1.loc[t1[5] == x]
@@ -121,14 +139,28 @@ def cluster(alldf, l):
 
             l1 = sortlist(np.array([dft1[6], dft1[7]]).T)
             l2 = sortlist(np.array([dft2[1], dft2[2]]).T)
-            print("SV found as reference", len(l2))
-            print("SV found as sample", len(l1))
+            print(y + "_" + x, file = sys.stderr)
+            print("SV found as reference", len(l2), file = sys.stderr)
+            print("SV found as sample", len(l1), file = sys.stderr)
             y_pred = fcluster(linkage(np.concatenate((l1, l2))), 10, criterion='distance')
             print(len(y_pred))
             print(len(set(y_pred)))
-            rr[y][x] = clusterthis(y_pred, dft1, dft2, l1, l2, len(l1))
+            rr[y][x] = clusterthis2(y_pred, dft1, dft2, l1, l2, len(l1))
             print("done\n")
     return rr
+
+def write(rr, prefix):
+    # Iterate over all genomes
+    for k1, v1 in rr.items():
+        with open(prefix + "_" + k1, "w") as file:
+            # Iterate over chromosomes
+            for k2, v2 in v1.items():
+                # Iterate over all clusters
+                for k3, v3 in v2.items():
+                    file.write("\t".join([k1 + "_" + k2, "\t".join([str(x) for x in v3[0][1]])]) + "\n")
+
+                    #file.write("\t".join([k1 + "_" + k2, "\t".join([str(x) for x in v3[0][1]])]) + "\t" + str(v3[0][1][1] - v3[0][1][0]) + "\t"
+                               #"\t".join([str(x) for x in v3[1:]]) + "\n")
 
 
 
@@ -138,13 +170,15 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--out", help="output file")
     parser.add_argument("-l", "--list", help = "list of all samples to merge")
     args = parser.parse_args()
+    print("testing")
 
     li = readList(args.list)
+    print("test2")
     df = pd.read_csv(args.input, sep = "\t", header = None)
+    print("test2")
     df = filter_syri(df)
-    cluster(df, li)
-    print(df)
-    print(li)
+    rr = cluster(df, li)
+    write(rr, args.out)
 
 
 
